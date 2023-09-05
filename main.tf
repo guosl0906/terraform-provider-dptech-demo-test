89168b9281d6f1df8a6af7b01e70fcd7859918b9
gsl <guosl_ilk@163.com>
2023-09-05 16:51:43 +0800

    添加main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    dptech-demo = {
+      source  = "xieguihua123/dptech-demo"
+      version = "1.2.38"
+    }
+  }
+}
+
+provider "dptech-demo" {
+  # Configuration options
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
