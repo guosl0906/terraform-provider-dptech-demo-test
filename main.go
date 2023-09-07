7a3a3dc0ec9cd2a48a2b3c47be2122222731a95d
gsl <guosl_ilk@163.com>
2023-09-08 00:02:23 +0800

    自定义

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
-	"log"
-
-	"terraform-provider-dptech-demo/provider"
-
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 var (
@@ -18,20 +11,4 @@ var (
 func main() {
 
 	fmt.Println("test content print")
-
-	var debug bool
-
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like ")
-	flag.Parse()
-
-	opts := providerserver.ServeOpts{
-		Address: "xieguihua123/dptech-demo",
-		Debug:   debug,
-	}
-
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 }
--- provider/provider.go
+++ provider/provider.go
@@ -3,99 +3,31 @@ package provider
 import (
 	"context"
 	"fmt"
-
-	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var _ provider.Provider = &ScaffoldingProvider{}
-
-type ScaffoldingProvider struct {
-	version string
-}
-
-type ScaffoldingProviderModel struct {
-	Name    types.String `tfsdk:"name"`
-	Address types.String `tfsdk:"address"`
-}
-
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "dptech-demo-123"
-	fmt.Println("111111111111111111111111111111111")
-}
-
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	fmt.Println("111111111111111111111111111111111")
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"name": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			}, "address": schema.StringAttribute{
-				MarkdownDescription: "  provider attribute",
-				Optional:            true,
-			},
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"myresource": resourceMyResource(),
 		},
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
-
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-	fmt.Println("111111111111111111111111111111111")
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	address := ""
-	if !data.Address.IsNull() {
-		address = data.Address.ValueString()
-	}
-
-	if data.Name.IsNull() {
-		tflog.Info(ctx, "Name is NULL")
-		return
-	}
-	if data.Address.IsNull() {
-		tflog.Info(ctx, "address is NULL")
-		return
-	}
-	tflog.Info(ctx, address)
-	client, err := NewClient(&address)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Unable to Create HashiCups API Client",
-			"An unexpected error occurred when creating the HashiCups API client. "+
-				"If the error is not clear, please contact the provider developers.\n\n"+
-				"HashiCups Client Error: "+err.Error(),
-		)
-		return
-	}
-
-	resp.DataSourceData = client
-	resp.ResourceData = client
-}
-
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{
-		NewExampleResource,
+func resourceMyResource() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: Create,
+		ReadContext:   Read,
 	}
 }
 
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
+func Create(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+	fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaa")
+	return nil
 }
 
-func New(version string) func() provider.Provider {
-	return func() provider.Provider {
-		return &ScaffoldingProvider{
-			version: version,
-		}
-	}
+func Read(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
+	fmt.Println("bbbbbbbbbbbbbbbbbbbbb")
+	return nil
 }
