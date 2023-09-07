bfd69737481ab79915705ad9f7010f7731feb273
gsl <guosl_ilk@163.com>
2023-09-07 15:46:19 +0800

    -debug-4

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     dptech-demo = {
-      source = "guosl0906/dptech-demo"
-      version = "0.1.6"
+      source = "xieguihua123/dptech-demo"
+      version = "1.2.38"
     }
   }
 }
